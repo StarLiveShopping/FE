38e23d035d8fbe60190844543e40c4e31ed030df
ii-0122 <76612998+ii-0122@users.noreply.github.com>
2025-01-08 00:30:42 +0900

    [STAR-21] chore: prettier 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,15 +10,19 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     settings: {
       react: {
-        version: "detect", // React 버전을 자동으로 감지
+        version: 'detect', // React 버전을 자동으로 감지
       },
     },
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'prettier/prettier': 'error',
+    },
+    plugins: {
+      prettier: pluginPrettier,
     },
   },
   { languageOptions: { globals: globals.browser } },
